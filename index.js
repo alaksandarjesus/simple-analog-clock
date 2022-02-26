e61b3eba90c0454df3cee6007ee6b8df1a0f39dc
Alaksandar Jesus Gene <alaksandarjesus@yahoo.co.in>
2022-02-26 22:57:41 +0530

    added demo link

--- index.js
+++ index.js
@@ -1,50 +1,65 @@
 const secondHandElement = document.querySelector(".second-hand");
 const minuteHandElement = document.querySelector(".minute-hand");
 const hourHandElement = document.querySelector(".hour-hand");
+const timestampElement = document.querySelector(".timestamp");
+const hours = timestampElement.querySelector(".hours");
+const minutes = timestampElement.querySelector(".minutes");
+const seconds = timestampElement.querySelector(".seconds");
 let secondHandAngle = 0;
 let hourHandAngle = 0;
 let minuteHandAngle = 0;
 
 const now = new Date();
 const hour = now.getHours();
-const hourFormatted = hour < 12? hour: (hour - 12);
-hourHandAngle = 30*hourFormatted;
+const hourFormatted = hour < 12 ? hour : (hour - 12);
+hourHandAngle = 30 * hourFormatted;
 
 const minute = now.getMinutes();
-minuteHandAngle = minute*6;
-
+minuteHandAngle = minute * 6;
 
 const second = now.getSeconds();
-secondHandAngle = second*6;
+secondHandAngle = second * 6;
 
 
-hourHandElement.style.transform =`rotate(${hourHandAngle}deg)`
-minuteHandElement.style.transform =`rotate(${minuteHandAngle}deg)`;
-secondHandElement.style.transform =`rotate(${secondHandAngle}deg)`;
+hourHandElement.style.transform = `rotate(${hourHandAngle}deg)`
+minuteHandElement.style.transform = `rotate(${minuteHandAngle}deg)`;
+secondHandElement.style.transform = `rotate(${secondHandAngle}deg)`;
 
+function updateTimestamp() {
+    const now = new Date();
+    const hour = now.getHours();
+    const hourFormatted = hour < 12 ? hour : (hour - 12);
+    hours.innerHTML = hourFormatted.toString().padStart(2, '0');
+    const minute = now.getMinutes();
+    minutes.innerHTML = minute.toString().padStart(2, '0');;
+    const second = now.getSeconds();
+    seconds.innerHTML = second.toString().padStart(2, '0');;
 
-setInterval(()=>{
-secondHandAngle = secondHandAngle + 6;
-if(secondHandAngle >= 360){
-    secondHandAngle = 0;
-    updateMinuteHand();
 }
-secondHandElement.style.transform =`rotate(${secondHandAngle}deg)`;
+
+setInterval(() => {
+    secondHandAngle = secondHandAngle + 6;
+    if (secondHandAngle >= 360) {
+        secondHandAngle = 0;
+        updateMinuteHand();
+    }
+    secondHandElement.style.transform = `rotate(${secondHandAngle}deg)`;
+    updateTimestamp();
 
 }, 1000)
 
-function updateMinuteHand(){
+function updateMinuteHand() {
     minuteHandAngle = minuteHandAngle + 6;
-    if(minuteHandAngle >= 360){
+    if (minuteHandAngle >= 360) {
         minuteHandAngle = 0;
         updateHourHand();
     }
-    minuteHandElement.style.transform =`rotate(${minuteHandAngle}deg)`;
+    minuteHandElement.style.transform = `rotate(${minuteHandAngle}deg)`;
 }
-function updateHourHand(){
+function updateHourHand() {
     hourHandAngle = hourHandAngle + 6;
-    if(hourHandAngle >= 360){
+    if (hourHandAngle >= 360) {
         hourHandAngle = 0;
     }
-    hourHandElement.style.transform =`rotate(${hourHandAngle}deg)`;
+    hourHandElement.style.transform = `rotate(${hourHandAngle}deg)`;
 }
